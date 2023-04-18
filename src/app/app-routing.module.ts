76a2eedd19112b659cfc57567bc55372425294b6
soufiane <bakkas280@gmail.com>
2023-04-18 10:46:42 +0000

    modification 18/04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Home/login/login.component';
 import {LoginAdminComponent} from "./Admin/login-admin/login-admin.component";
+import {SidebarComponent} from "./Admin/interfaces-admin/sidebar/sidebar.component";
+import {CategorieComponent} from "./Admin/interfaces-admin/categorie/categorie.component";
+import {ClientComponent} from "./Admin/interfaces-admin/client/client.component";
+import {FirstpageComponent} from "./Home/firstpage/firstpage.component";
+import {PanierComponent} from "./Home/panier/panier.component";
 
 const routes: Routes = [
+  {path :"**", component :LoginComponent},
   {path :"login", component :LoginComponent},
   {path :"admin", component :LoginAdminComponent},
+  {path :"admin/sidebar", component :SidebarComponent},
+  {path :"admin/categorie", component :CategorieComponent},
+  {path :"admin/client", component :ClientComponent},
+  {path :"home", component :FirstpageComponent},
+  {path :"panier", component :PanierComponent},
+
+
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { FirstpageComponent } from './Home/firstpage/firstpage.component';
 import { LoginComponent } from './Home/login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginAdminComponent } from './Admin/login-admin/login-admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './Admin/interfaces-admin/sidebar/sidebar.component';
+import { CategorieComponent } from './Admin/interfaces-admin/categorie/categorie.component';
+import { ClientComponent } from './Admin/interfaces-admin/client/client.component';
+import { PanierComponent } from './Home/panier/panier.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,16 @@ import { LoginAdminComponent } from './Admin/login-admin/login-admin.component';
     FirstpageComponent,
     LoginComponent,
     LoginAdminComponent,
+    SidebarComponent,
+    CategorieComponent,
+    ClientComponent,
+    PanierComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
